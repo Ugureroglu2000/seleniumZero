1732af57f33563ade14b3d922e2d82fee17bdad9
Ugureroglu2000 <ugureroglu2000@gmail.com>
2020-12-16 08:07:47 +0000

    g

--- src/test/java/seleniumZero/Testbase.java
+++ src/test/java/seleniumZero/Testbase.java
@@ -25,7 +25,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-//Ugur branc ugur github ediited master
+//Ugur branc ugur github ediited master editted git master again
 
 public class Testbase {
     public static WebDriver driver;
